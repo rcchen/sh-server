3c55257f54b487486a42dd1e18bf58fa9c4f4142
Roger Chen <rogerthechen@gmail.com>
2015-01-17 17:20:09 -0500

    Added Bundler, moved dependencies, set up config.ru

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'bson_ext'
+gem 'json'
+gem 'mongo'
+gem 'mongo_mapper'
+gem 'sinatra'
+gem 'unicorn'
--- server.rb
+++ server.rb
@@ -1,50 +1,53 @@
-require 'json'
-require 'mongo'
-require 'mongo_mapper'
-require 'sinatra'
-
 require './models/user'
 require './models/photo'
 
-MongoMapper.connection = Mongo::Connection.new('localhost')
-MongoMapper.database = 'selfiehero'
+class Server < Sinatra::Base
 
-# Create a new user
-post '/api/users' do
-	content_type :json
-	user = User.create({
-		:email => params[:email],
-		:token => SecureRandom.urlsafe_base64(16)
-	})
-	user.to_json
-end
+	MongoMapper.connection = Mongo::Connection.new('localhost')
+	MongoMapper.database = 'selfiehero'
 
-# Show photos
-get '/api/photos' do
-	content_type :json
-	Photo.all.to_json
-end
-
-# Add photo
-post '/api/photos' do
-	filename = "public/#{SecureRandom.uuid}.jpeg"
-	File.open(filename, "w") do |f|
-		f.write(params[:photo][:tempfile].read)
+	# Basic route
+	get '/' do
+		'<pre>Server is operational.</pre>'
 	end
-	photo = Photo.create({ :url => filename, :latitude => latitude, :longitude => longitude })
-	user = User.first( :token => params[:token] )
-	user.photos << photo
-	photo.to_json
-end
 
-# Heart/unheart a photo
-post '/api/photos/:id/like' do
-	photo = Photo.first(:id => params[:id])
-	user = User.first(:token => params[:token])
-	if photo[:hearts].include? user[:_id]
-		photo[:hearts].delete(user[:_id])
-	else
-		photo[:hearts] << user[:_id]
+	# Create a new user
+	post '/api/users' do
+		content_type :json
+		user = User.create({
+			:email => params[:email],
+			:token => SecureRandom.urlsafe_base64(16)
+		})
+		user.to_json
 	end
-	photo.save!
+
+	# Show photos
+	get '/api/photos' do
+		content_type :json
+		Photo.all.to_json
+	end
+
+	# Add photo
+	post '/api/photos' do
+		filename = "public/#{SecureRandom.uuid}.jpeg"
+		File.open(filename, "w") do |f|
+			f.write(params[:photo][:tempfile].read)
+		end
+		photo = Photo.create({ :url => filename, :latitude => latitude, :longitude => longitude })
+		user = User.first( :token => params[:token] )
+		user.photos << photo
+		photo.to_json
+	end
+
+	# Heart/unheart a photo
+	post '/api/photos/:id/like' do
+		photo = Photo.first(:id => params[:id])
+		user = User.first(:token => params[:token])
+		if photo[:hearts].include? user[:_id]
+			photo[:hearts].delete(user[:_id])
+		else
+			photo[:hearts] << user[:_id]
+		end
+		photo.save!
+	end
 end
